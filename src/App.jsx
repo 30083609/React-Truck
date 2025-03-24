59f33f6d3f2d04b2adcf575c71d69245d926b54b
30083609 <30083609@student.toiohomai.ac.nz>
2025-03-25 11:12:42 +1300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import {useState, useEffect} from 'react';
+import Nav from './Nav';
+import Truck from './Truck';
+
 
 function App() {
-    return(
-      <h1>Bare bones react application</h1>
-    );
+
+  const [trucks, setTrucks] = useState([]);
+  const [selectedTruck, setSelectedTruck] = useState(null);
+
+  useEffect(
+    () => {
+      fetch("src/data.json")
+        .then(response => response.json())
+        .then(data => setTrucks(data)) 
+        .catch(error => console.error("Error loading data", error))
+    }, []
+
+  )
+
+  return (
+    <div>
+      <h1>Trucks</h1>
+      <Nav trucks={trucks} onTruckSelect={setSelectedTruck} />
+      <Truck truck={selectedTruck} />
+    </div>
+  );
+
 }
 
 export default App
